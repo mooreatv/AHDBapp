7de728361f286e82d332bd39a58311dffcf9ea4e
Moorea Tv <moorea@ymail.com>
2019-09-06 16:03:50 -0700

    bootstrap
    does nothing so far

---
+++ ahdb.go
@@ -0,0 +1,19 @@
+// Copyright 2019 MooreaTv moorea@ymail.com
+// All Rights Reserved
+//
+// GPLv3 License (which means no commercial integration)
+// ask if you need a different License
+//
+
+package main
+
+import (
+	"flag"
+
+	"fortio.org/fortio/log"
+)
+
+func main() {
+	flag.Parse()
+	log.Infof("AHDB parser started...")
+}

305598c1eeec7591985d18c5089669992ada7db9
Moorea Tv <moorea@ymail.com>
2019-09-06 20:42:55 -0700

    Initial CSV export POC/wip

--- ahdb.go
+++ ahdb.go
@@ -8,12 +8,39 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
+	"fmt"
+	"os"
 
 	"fortio.org/fortio/log"
 )
 
+// ScanEntry is 1 auction house scan result
+type ScanEntry struct {
+	DataFormatVersion int
+	Ts                int
+	Count             int
+	ItemDBCount       int
+	ItemsCount        int
+}
+
+// JSONData is toplevel structure produced by ahdbSavedVars2Json
+type JSONData struct {
+	Ah []ScanEntry
+}
+
 func main() {
 	flag.Parse()
 	log.Infof("AHDB parser started...")
+	var ahdb JSONData
+	if err := json.NewDecoder(os.Stdin).Decode(&ahdb); err != nil {
+		log.Fatalf("Unable to unmarshal json result from stdin: %v", err)
+	}
+	fmt.Println(`"Version","Ts","Count","ItemDBCount","ItemsCount"`)
+	for idx := range ahdb.Ah {
+		entry := ahdb.Ah[idx]
+		fmt.Printf("%d,%d,%d,%d,%d\n", entry.DataFormatVersion, entry.Ts, entry.Count, entry.ItemDBCount, entry.ItemsCount)
+	}
+	log.Infof("Done, found %d scans", len(ahdb.Ah))
 }

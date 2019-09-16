7c04a3b0d8bafb7e9560323ffdf379cdb07f6626
Moorea Tv <moorea@ymail.com>
2019-09-15 18:50:45 -0700

    conversion of sed+awk script to golang

---
+++ lua2json/lua2json.go
@@ -0,0 +1,153 @@
+// Copyright 2019 MooreaTv moorea@ymail.com
+// All Rights Reserved
+//
+// GPLv3 License (which means no commercial integration)
+// ask if you need a different License
+//
+// This is the conversion of my lua2json.sh:
+// Crude Lua table (wow saved variables) to Json "converter"
+//
+// go run lua2json < "C:\Program Files (x86)\World of Warcraft\_classic_\WTF\Account\$ACCT\SavedVariables\AuctionDB.lua" > auctiondb.json
+//
+// If you don't like regular expressions, don't look further :)
+//
+// In order, sed expressions:
+// Add "" around toplevel array names
+// Remove -- comments
+// Change = to :
+// Change ["foo"] to "foo"
+// Change [123] to "123" (keys in json can only be strings)
+// Change nil array keys to null
+// Then Awk to remove trailing coma and turn list to arrays
+// NOTE: anchors/quote boundaries are important to not replace inside the middle of a string value
+
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"regexp"
+	"strings"
+
+	"fortio.org/fortio/log"
+)
+
+// RegsubInput is the input pattern+replacement string (or pattern)
+type RegsubInput struct {
+	Find      string
+	ReplaceBy string
+}
+
+// Regsub is a pattern+replacement string (or pattern), with the compile regular expression
+type regsub struct {
+	find      *regexp.Regexp
+	replaceBy string
+}
+
+/*
+sed -E -e 's/^([^": }\t]+)/"\1"/' \
+    -e "s/ -- .*$//g" \
+    -e "s/ = /: /g" \
+    -e 's/\["/"/g' \
+    -e 's/\"]/"/g' \
+    -e 's/^([ \t]*)\[([0-9.]+)\]:/\1"\2":/' \
+    -e 's/^([ \t]*)nil,$/\1null,/'
+*/
+var rei = []RegsubInput{
+	{`^([^": }\t]+)`, `"$1"`},
+	{` -- .*$`, ""},
+	{` = `, `: `},
+	{`\["`, `"`},
+	{`\"]`, `"`},
+	{`^([ \t]*)\[([0-9.]+)\]:`, `$1"$2":`},
+	{`^([ \t]*)nil,$`, `${1}null,`},
+}
+
+// changes trailing braces into trailing bracket
+func brace2bracket(line string) string {
+	lastPos := len(line) - 1
+	if lastPos >= 0 && line[lastPos] == '{' {
+		return line[0:lastPos] + "["
+	}
+	return line
+}
+
+// Lua2Json stream converts a simple wow lua saved variables to json
+func Lua2Json(in io.Reader, out io.Writer, bufSizeMb float64) {
+	re := make([]regsub, len(rei))
+	for i, r := range rei {
+		re[i].find = regexp.MustCompile(r.Find)
+		re[i].replaceBy = r.ReplaceBy
+	}
+	scanner := bufio.NewScanner(in)
+	sz := int(bufSizeMb * 1024 * 1024)
+	log.Infof("Using buffer size %d", sz)
+	buf := make([]byte, 0, sz)
+	scanner.Buffer(buf, sz)
+	numLines := 0
+	out.Write([]byte("{\n"))
+	// BEGIN {startnest=0; inarray=0}
+	startNest := false
+	inArray := false
+	trailingCommaFind := regexp.MustCompile(`},?$`)
+	colonFind := regexp.MustCompile(`^[^:]+$`)
+	prevLine := ""
+	for scanner.Scan() {
+		line := scanner.Text()
+		numLines = numLines + 1
+		for _, r := range re {
+			line = r.find.ReplaceAllString(line, r.replaceBy)
+
+		}
+		// Awk conversion section:
+		//		/},?$/ {gsub(",$", "", l); if (inarray) gsub("}", "]"); inarray=0}
+		if trailingCommaFind.MatchString(line) {
+			prevLine = prevLine[0 : len(prevLine)-1]
+			if inArray {
+				line = strings.ReplaceAll(line, "}", "]")
+			}
+			inArray = false
+		}
+		//		/^[^:]+$/ {if (startnest) gsub("{$", "[", l); startnest=0; inarray=1}
+		if colonFind.MatchString(line) {
+			if startNest {
+				prevLine = brace2bracket(prevLine)
+			}
+			startNest = false
+			inArray = true
+		}
+		//		/: / {inarray=0}
+		if strings.Index(line, ": ") >= 0 {
+			inArray = false
+		}
+		//		/{$/ {startnest=1}
+		lastPos := len(line) - 1
+		if lastPos >= 0 && line[lastPos] == '{' {
+			startNest = true
+		}
+		//		{if (l) print l; l=$0}
+		if prevLine != "" {
+			fmt.Fprintln(out, prevLine)
+		}
+		prevLine = line
+	}
+	fmt.Fprintln(out, prevLine) // 	   END {print l}
+	out.Write([]byte("}\n"))
+	if err := scanner.Err(); err != nil {
+		log.Errf("error scanning: %v", err)
+	}
+	log.Infof("Done, %d lines converted", numLines)
+}
+
+var (
+	buffSize = flag.Float64("s", 16, "Buffer size in Mbytes")
+)
+
+func main() {
+	flag.Parse()
+	log.Infof("AHDB lua2json started...")
+	Lua2Json(os.Stdin, os.Stdout, *buffSize)
+}
